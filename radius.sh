3f986f9df7db2c21ecfa92fdba37dedf0ce15445
it-toppp <36742579+it-toppp@users.noreply.github.com>
2020-06-15 00:01:16 +0300

    Create radius.sh

---
+++ radius.sh
@@ -0,0 +1,46 @@
+
+VPNIP=
+RADPASS=
+MYPASS=
+VPNIP=
+
+yum -y install freeradius freeradius-mysql freeradius-utils mysql mysql-devel mysql-server
+systemctl start mysqld && systemctl enable mysqld
+
+MYPWD=byrfgcekzwbz
+sudo mysql_secure_installation  <<MSI2
+
+y
+${MYPASS}
+${MYPASS}
+y
+y
+y
+y
+
+MSI2
+
+systemctl restart mysqld
+mysql -uroot –p$MYPASS -e "CREATE DATABASE radius DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;"
+mysql -uroot -p$MYPASS radius < /etc/raddb/mods-config/sql/main/mysql/schema.sql /etc/raddb/mods-config/sql/main/mysql
+ 
+cat > /etc/raddb/clients.conf << HERE
+client server-1 {
+    	ipaddr      	= $VPNIP
+     	secret      	= $RADPASS
+     	shortname   	= server-1 
+}
+HERE
+   
+cd /etc/raddb/mods-available/
+ln -s ../mods-available/inner-eap ../mods-enabled/inner-eap
+
+systemctl restart radiusd
+
+yum -y install firewalld
+systemctl enable firewalld
+firewall-cmd --permanent --zone=public --add-rich-rule='rule family="ipv4" port="3306/tcp" source address="SITEIP/32" accept'
+firewall-cmd --permanent --new-ipset=vpnservers --type=hash:ip
+firewall-cmd --ipset=vpnservers --add-entry=$VPNIP
+firewall-cmd --permanent --zone=public --add-rich-rule='rule family="ipv4" port="1812-1813/udp" source ipset=vpnservers accept'
+firewall-cmd --reload
